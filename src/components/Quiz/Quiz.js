5d4833a8e2501e2be4673e65dc4b7b5c9f532284
AmirHossein <amirhossian23@gmail.com>
2025-09-28 10:43:26 +0330

    quiz div

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -19,10 +19,63 @@ export default class Quiz extends React.Component {
 
                     ]
                 },
-                
-            ]
+                {
+                    questionText : 'Who is CEO of Tesla?',
+                    answerOptions :[
+                        {answerText : 'Jeff Bezos' , isCorrect : false},
+                        {answerText : 'Elon Musk' , isCorrect : true},
+                        {answerText : 'Bill Gates' , isCorrect : false},
+                        {answerText : 'Toney Stark' , isCorrect : false},
+
+                    ],
+                },
+                {
+                    questionText : "the iphone created by which company?",
+                    answerOptions : [
+                        {answerText : 'Apple' , isCorrect : true},
+                        {answerText : 'Intel' , isCorrect : false},
+                        {answerText : 'Amazon' , isCorrect : false},
+                        {answerText : 'Microsoft' , isCorrect : false},
+                        
+                    ],
+                },
+                {
+                    questionText : "How many UCL does AC Milan have?",
+                    answerOptions : [
+                        {answerText : '7' , isCorrect : true},
+                        {answerText : '5' , isCorrect : false},
+                        {answerText : '6' , isCorrect : false},
+                        {answerText : '3' , isCorrect : false},
+                        
+                    ],
+                }
+
+            ],
+            currentQuestion : 0 ,
+            showScore : false , 
+            score : 0
         }
     
+    }
+    render (){
+        return (
+            <div className="app">
+                <div className="score-section"></div>
+                <div className="question-section">
+                    <div className="question-count">
+                        <span>Question 1</span>/4
+                    </div>
+                    <div className="question-text"></div>
+
+                </div>
+                <div className="answer-section">
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                </div>
+            </div>
+        )
     }
     
 }
